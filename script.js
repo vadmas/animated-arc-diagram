5b38dfd1c95bb8fe795f09c6ba718a22fd6e05b1
Vaden Masrani <vadmas@gmail.com>
2015-08-24 22:00:52 -0700

    Updated the scaffolding to follow bostock's reusability convention

--- script.js
+++ script.js
@@ -1,14 +1,66 @@
-(function() {
-vis = {}
-vis.init = function(params) {
-  // code for my init function ...
-  vis.loaddata(params);
-}
-vis.loaddata = function(params) {
-  // code for loading data ...
-  vis.draw(params);
-}
-vis.draw = function(params) {
-  // code for drawing stuff ...
-}
-})();
+function chart (){
+	// Globals
+	// eg
+	// var margin = {top: 20, right: 20, bottom: 20, left: 20},
+	//     width = 720, // default width
+	//     height = 120,
+	// 	xValue = function(d){return d[0]},
+	// 	yValue = function(d){return d[1]},
+	//     xScale = d3.time.scale(),
+	// 	yScale = d3.scale.linear(),
+	// 	xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickSize(6, 0),
+
+    // Main closure
+	function chart(selection){
+		selection.each(function(data){
+			
+		// Make chart here
+
+		});
+	}
+
+	// Helper functions here
+	// Eg
+	// The x-accessor for the path generator; xScale ∘ xValue.
+	// function X(d) {
+	// 	return xScale(d[0]);
+	// 	}
+	// function Y(d) {
+	// 	return yScale(d[1]);
+	// 	}
+
+	// Getter/setter methods
+	// Eg
+	// chart.margin = function(_) {
+	//   if (!arguments.length) return margin;
+	//   margin = _;
+	//   return chart;
+	// };
+
+
+	// chart.width = function(_) {
+	//   if (!arguments.length) return width;
+	//   width = _;
+	//   return chart;
+	// };
+
+	// chart.height = function(_) {
+	//   if (!arguments.length) return height;
+	//   height = _;
+	//   return chart;
+	// };
+
+	// chart.x = function(_) {
+	//    if (!arguments.length) return xValue;
+	//    xValue = _;
+	//    return chart;
+	//  };
+
+	// chart.y = function(_) {
+	//   if (!arguments.length) return yValue;
+	//   yValue = _;
+	//   return chart;
+	// };
+
+	return chart;
+	}
